d4df3d74b330cbe519cec33c122bf6a52bd559a0
Mislav Perić <mi.peric@reply.de>
2025-01-04 18:36:11 +0100

    added a repl

--- main.go
+++ main.go
@@ -1,1 +1,20 @@
-package gemlang
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/user"
+
+	"github.com/mislavperi/gem-lang/repl"
+)
+
+func main() {
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Hello %s! This is the Gem programming lanauage!\n", user.Username)
+	fmt.Printf("Feel free tp type in some commands\n")
+	repl.Start(os.Stdin, os.Stdout)
+}
